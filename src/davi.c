141e382ef5cc5a3cb63fe71e855bdf40aa01a7d0
davigbra <113560548+davigbra@users.noreply.github.com>
2022-12-02 19:20:09 -0300

    Update davi.c

--- src/davi.c
+++ src/davi.c
@@ -1,71 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include<string.h>
 #include <conio.h>
 #include <locale.h>
-
-void addMedico(FILE *f);
-int localizaMedico(FILE *f, int codigo);
 
 int main()
 {
     FILE *f;
     char op;
-    setlocale(LC_ALL, "portuguese");
-    if ((f = fopen("teste.dat", "r+b")) == NULL)
+    setlocale(LC_ALL,"portuguese");
+    if ((f = fopen("teste.dat", "r+b"))==NULL)
     {
         printf("Arquivo não existia ... criando arquivo!");
-        if ((f = fopen("teste.dat", "w+b")) == NULL) // arq não existe
+        if((f = fopen("teste.dat", "w+b"))==NULL) //arq não existe
         {
             printf("Erro na criação do arquivo!!");
             exit(1);
         }
         system("pause");
     }
+
     addMedico(f);
+
+
+
 }
 
 struct CMedico
 {
     int codigo;
     char nome[40];
-    unsigned long long int numero;
+    char numero[20];
     char especialidade[20];
+
 };
 typedef struct CMedico medico;
-
-int localizaMedico(FILE *f, int codigo)
+int localizaMedico(FILE* f, int codigo)
 {
-    int posicao = -1;
-    int encontrado = 0;
+    int posicao=-1;
+    int encontrado=0;
     medico med;
-    fseek(f, 0, SEEK_SET);
-    fread(&med, sizeof(med), 1, f);
-    while (!feof(f) && !encontrado)
+    fseek(f,0,SEEK_SET);
+    fread(&med, sizeof(med),1,f);
+    while(!feof(f) && !encontrado)
     {
-        if (med.codigo == codigo)
+        if(med.codigo==codigo)
         {
-            encontrado = 1;
+            encontrado=1;
         }
-        fread(&med, sizeof(med), 1, f);
+        fread(&med,sizeof(med),1,f);
+
+
     }
-    if (encontrado == 1)
+    if(encontrado==1)
     {
         return posicao;
+
     }
     else
         return -1;
 }
-
-void addMedico(FILE *f)
+void addMedico(FILE*f)
 {
     medico med;
     int posicao;
     printf("\nDigite o código do medico a ser cadastrado\n");
     fflush(stdin);
     scanf("%d", &med.codigo);
-    posicao = localizaMedico(f, med.codigo);
-    if (posicao == -1)
+    posicao=localizaMedico(f,med.codigo);
+    if (posicao==-1)
     {
         printf("Nome do medico:\n");
         fflush(stdin);
@@ -73,11 +76,37 @@ void addMedico(FILE *f)
         printf("Especialidade\n");
         gets(med.especialidade);
         printf("Numero, formato DDD+Numero (sem 0 do DDD)\n");
-        scanf("%llu", &med.numero);
-        fseek(f, 0, SEEK_END);
-        fwrite(&med, sizeof(med), 1, f);
+        gets(med.numero);
+        fseek(f,0,SEEK_END);
+        fwrite(&med,sizeof(med),1,f);
         fflush(stdin);
+
+
+
+
     }
     else
         printf("Codigo do medico ja existente. Nao foi possivel realizar inclusao");
+
+
+
+
 }
+/*
+void imprime_Medico(FILE *f)
+{
+    medico m;
+    fseek(f,0,SEEK_SET);
+    fread(&m, sizeof(m),1, f);
+    while (!feof(f))
+    {
+        printf("Código....:%d \n",m.codigo);
+        printf("Nome.:%s \n",m.nome);
+        printf("Numero.....:%s\n",m.numero);
+        printf("Especialidade.....:%s\n",m.especialidade);
+
+        fread(&m, sizeof(m),1, f);
+    }
+}
+
+*/
