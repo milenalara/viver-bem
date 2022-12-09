d7a6e51114067ccff099df1d0b2c1343fb817307
Milena Lara <milenalrf@gmail.com>
2022-12-08 21:53:20 -0300

    Atualizando menu

--- src/main.c
+++ src/main.c
@@ -4,20 +4,33 @@
 #include <locale.h>
 #include <time.h>
 
-void chooseProgram(int op);
 void menu();
-void cadastrarPaciente();
-void cadastrarMedico();
-void cadastrarConsulta();
-void realizarPesquisas();
+int localizaConsulta(FILE *f, int codigo);
+int localizaMedicoConsulta(FILE *f, int codigo, int dia, int mes, int ano);
+int localizaQtdConsulta(FILE *f, int codigo, int dia, int mes, int ano);
+int localizaQtdConsulta(FILE *f, int codigo, int dia, int mes, int ano);
+int localizaMedico(FILE *f, int codigo);
+void addMedico(FILE *f);
+void imprime_Medico(FILE *f);
+void imprime_Consulta(FILE *f);
+int localizaPaciente(FILE *f, int codigo);
+void incluiPaciente(FILE *f);
+void alteraPaciente(FILE *f);
+void listaPacientes(FILE *f);
+void cadastraConsulta(FILE *arqmedico, FILE *arqpaciente, FILE *arqconsulta);
+void imprimeConsultaPorData(FILE *f, FILE *p, FILE *m);
+void imprimeConsultaPorMedico(FILE *f, FILE *p, FILE *m);
+void excluiConsulta(FILE *f, int codigo);
+int pagamentoMedico(FILE *arqconsulta, FILE *arqmedico, FILE *f);
+void cancelaConsulta(FILE *arqconsulta);
+
+FILE *fPacientes, *fMedicos, *fConsultas;
 
 int main()
 {
   setlocale(LC_ALL, "Portuguese");
   srand(time(NULL));
   int op;
-
-  void chooseProgram(int op);
 
   do
   {
@@ -27,34 +40,32 @@ int main()
     printf("OU digite -1 para sair");
     scanf("%i", &op);
 
-    void chooseProgram(int op);
+    switch (op)
+    {
+    case -1:
+      break;
+    case 0:
+      menu();
+      break;
+    case 1:
+      incluiPaciente(&fPacientes);
+      break;
+    case 2:
+      addMedico(&fMedicos);
+      break;
+    case 3:
+      cadastraConsulta(&fMedicos, &fPacientes, &fConsultas);
+      break;
+    case 4:
+      break;
+    default:
+      printf("Programa não encontrado\n");
+      break;
+    }
+
   } while (op != -1);
 
   return 0;
-}
-
-void chooseProgram(int op)
-{
-  switch (op)
-  {
-  case -1:
-    break;
-  case 0:
-    menu();
-    break;
-  case 1:
-    cadastrarPaciente();
-    break;
-  case 2:
-    cadastrarMedico();
-    break;
-  case 3:
-    cadastrarConsulta();
-    break;
-  default:
-    printf("Programa não encontrado\n");
-    break;
-  }
 }
 
 void menu()
@@ -62,21 +73,7 @@ void menu()
   printf("1 - cadastrar um paciente");
   printf("2 - cadastrar um médico");
   printf("3 - cadastrar uma consulta");
-  printf("4 - realizar pesquisas");
-}
-
-void cadastrarPaciente()
-{
-}
-
-void cadastrarMedico()
-{
-}
-
-void cadastrarConsulta()
-{
-}
-
-void realizarPesquisas()
-{
+  printf("4 - buscar paciente");
+  printf("5 - buscar médico");
+  printf("4 - buscar consulta");
 }
